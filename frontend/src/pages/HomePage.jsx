94910faf6962d12718da7035a6826f0c3349a904
Rahat Chowdhury <rahatchowdhury@Rahats-MacBook-Pro.local>
2023-02-21 23:29:09 -0700

    Land Page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import musician from "../assets/Musician.png"
+
+const BUTTON_CONTAINER = "bg-primary w-64 text-sm px-3 py-3 rounded-lg"
 
 const HomePage = () => {
 	return (
 		<>
-			<Navbar/>
-			<h1 className="font-ProximaNova text-8xl">home page</h1>
-			<p>welcome to the homepage</p>
+			<div className="bg-dark text-white min-h-screen items-center">
+				<Navbar/>
+				<div className="flex flex-row justify-center mt-36 space-x-24">
+					<img className=" " src={musician}/>
+					<div className="flex flex-col space-y-4">
+						<p className="w-[350px] font-thin text-xl">
+							Welcome to ConcertCopper, your one 
+							stop shop for purchasing concert tickets
+							for your favourite artists!
+						</p>
+						<button className={BUTTON_CONTAINER}>SEARCH AVAILABLE VENUES</button>
+					</div>
+				</div>
+			</div>
 		</>
+
+
 	)
 }
 export default HomePage;
