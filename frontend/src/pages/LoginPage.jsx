74758c056d259987fb708c3a16d8dd70f504d3e2
chowdhuryrahat2 <chowdhuryrahat2@gmail.com>
2023-03-09 16:59:05 -0700

    Added login structure

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,52 +1,45 @@
 import React, {useState} from "react";
 const BUTTON_CONTAINER = "bg-primary w-96 m-auto text-xl mt-6 px-15 py-3 rounded-lg hover:text-dark ease-in duration-300 font-bold"
 
-const FORM_LABEL = "text-xl text-dark/50 font-semibold";
-const FORM_CONTAINER = "flex flex-col gap-1";
-const FORM_FIELD = "h-10 font-semibold text-lg pl-1 text-dark";
+const FORM_LABEL = "";
+const FORM_CONTAINER = "";
+const FORM_FIELD = "";
 
 function LoginPage() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [account, setAccount] = useState({
+        email: "",
+        password: ""
+    });
 
-    const handleInputChange = (event) => {
-        const {name, value} = event.target;
-        if (name == "Email"){
-            setEmail(value);
-        } else if (name == "Password"){
-            setPassword(value);
-        }
+    const handleClick = (event) => {
+        event.preventDefault();
+        console.log("logged in!", account);
     };
 
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        console.log(`Email: ${email}, Password: ${password}`);
-    }; 
+    const handleChange = (event) => {
+        setAccount((prevState) => {
+            return {
+                ...prevState,
+                [event.target.name]: event.target.value
+            };
+        })
+    }
             
 	return ( 
         <>
- 			<div className="bg-dark text-white min-h-screen ">
-                <div className="flex flex-col w-96 mx-auto">   
-                    <div className="bg-white w-96 mt-72 space-y-10 ">
-                        <h1 className="text-2xl text-dark">
-                            Login
-                        </h1>
-                        <form onSubmit = {handleFormSubmit}>
-						<div className={FORM_CONTAINER}>
-							<label className={FORM_LABEL}>Email</label>
-                            <input className={FORM_FIELD} type ="text" name = "Email" required value ={email} onChange = {handleInputChange}/>
-						</div>
-						<div className={FORM_CONTAINER}>
-							<label className={FORM_LABEL}>Password</label>
-                            <input className={FORM_FIELD} type ="password" name = "Password" required value = {password} onChange = {handleInputChange} />
-						</div>
-                        <button className={BUTTON_CONTAINER} >
-                                Log In
-                        </button>
-					</form>
+ 			<div className="flex flex-col bg-dark min-h-screen text-white items-center justify-center">
+                <div className="flex flex-col">
+                    <h1>Login</h1>
+                    <div className="px-8 bg-white w-[500px] h-80">
+                        <form className="pt-6 flex flex-col text-dark gap-4">
+                            <label>Email</label>
+                            <input onChange={(e) => handleChange(e)} name="email" type="text"/>
+                            <label>Password</label>
+                            <input onChange={(e) => handleChange(e)} name="password" type="password"/>
+                            <button onClick={(e) => handleClick(e)}>LOGIN</button>
+                        </form>
                     </div>
                 </div>
-
 			</div>           
         
         </>
