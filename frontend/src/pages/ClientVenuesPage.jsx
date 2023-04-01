35e299ee7d91f8056298dea4c1c4ce61c6931ad4
chowdhuryrahat2 <chowdhuryrahat2@gmail.com>
2023-04-01 14:08:55 -0600

    Scrollbar adjusted

--- frontend/src/pages/ClientVenuesPage.jsx
+++ frontend/src/pages/ClientVenuesPage.jsx
@@ -6,42 +6,40 @@ import Amalie from "../assets/Amalie.jpg";
 import Climate from "../assets/Climate.jpeg";
 
 const data = [
-	{
-		name: "Scotiabank Saddledome",
-		location: "Calgary, AB",
-		img: Saddledome
-	},
-	{
-		name: "T-Mobile Arena",
-		location: "Paradise, NV",
-		img: TMobile
-	},
-	{
-		name: "Amalie Arena",
-		location: "Tampa, FL",
-		img: Amalie
-	},
-	{
-		name: "Climate Pledge Arena",
-		location: "Seattle, WA",
-		img: Climate
-	},
+    {
+        name: "Scotiabank Saddledome",
+        location: "Calgary, AB",
+        img: Saddledome
+    },
+    {
+        name: "T-Mobile Arena",
+        location: "Paradise, NV",
+        img: TMobile
+    },
+    {
+        name: "Amalie Arena",
+        location: "Tampa, FL",
+        img: Amalie
+    },
+    {
+        name: "Climate Pledge Arena",
+        location: "Seattle, WA",
+        img: Climate
+    },
 ]
 function ClientVenuesPage() {
-	return ( 
-		<div className="flex flex-col bg-dark min-h-screen w-max text-white items-center justify-center">
-			{/* Page container */}
-			<div className="flex flex-col justify-center gap-1 text-2xl px-12">
-				<h1>AVAILABLE VENUES</h1>
-				{/* Carousel */}
-				<div className="flex flex-row overflow-scroll gap-12 mx-auto">
-					{data.map((data, index) => (
-						<VenueCard key={index} {...data}/>
-					))}
-				</div>
-			</div>
-		</div>
-	 );
+    return ( 
+        <div className="bg-dark min-h-screen text-white">
+            <div className="flex flex-col justify-center gap-1 text-2xl px-12 pt-48">
+                <h1>AVAILABLE VENUES</h1>
+                <div className="flex flex-row scrollbar-hide overflow-x-scroll gap-x-24">
+                    {data.map((data, index) => (
+                        <VenueCard key={index} {...data}/>
+                    ))}
+                </div>
+            </div>
+        </div>
+     );
 }
 export default ClientVenuesPage;
 
